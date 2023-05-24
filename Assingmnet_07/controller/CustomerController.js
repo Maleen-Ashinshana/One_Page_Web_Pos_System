5b11bd8dc695986b4fbc730eb901c5e4a5a63a0a
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-05-24 23:23:06 +0530

    add customer part1

--- Assingmnet_07/controller/CustomerController.js
+++ Assingmnet_07/controller/CustomerController.js
@@ -3,3 +3,25 @@ import{customerModel} from "../model/customerModel";
 $('').on('click','',function (event) {
     console.log($(event.target).text());
 })
+$('#').on('click',(event)=>{
+    let value=$('input[name=""]').val();
+    if (value){
+        console.log(value);
+        $('').append(<li>${value}</li>)
+    }else{
+        alert('invalid-input')
+    }
+})
+
+function saveCustomer() {
+    let  Customer_Id=$('#customer_id').val();
+    let  Customer_name=$('#customer_name').val();
+    let  Customer_address=$('#customer_address').val();
+    let  Customer_salary=$('#customer_salary').val();
+
+    var tableData=`<tr>
+                  <th scope="row">${cus}</th>   
+                   
+    
+    `
+}
