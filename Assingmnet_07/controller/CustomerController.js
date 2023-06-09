43a41997dca68381d5371e7f87d9f5a7fe180a34
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-06-09 11:50:23 +0530

    add the sintax

--- Assingmnet_07/controller/CustomerController.js
+++ Assingmnet_07/controller/CustomerController.js
@@ -1,5 +1,6 @@
-import{customerModel} from "../model/customerModel";
+/*import{customerModel} from "../model/customerModel.js";*/
 
+/*
 $('').on('click','',function (event) {
     console.log($(event.target).text());
 })
@@ -20,8 +21,33 @@ function saveCustomer() {
     let  Customer_salary=$('#customer_salary').val();
 
     var tableData=`<tr>
-                  <th scope="row">${cus}</th>   
-                   
+                   <td>${Customer_Id}</td>
+                   <td>${Customer_name}</td>
+                   <td>${Customer_address}</td>
+                   <td>${Customer_salary}</td>
+                   </tr>`
+        var tableDT=`<option value="${Customer_name}">`
     
     `
+}*/
+
+import{CustomerModel} from "../model/customerModel.js";
+export class CustomerController{
+    constructor() {
+        /*Buttone Click tika danne meke*/
+        $('#btnAddCustomer').click(this.handleSaveCustomer);
+        $('#btnUpdateCustomer').click(this.handleUpdateCustomer);
+        $('#btnDeleteCustomer').click(this.handleDeleteCustomer);
+    }
+
+    handleSaveCustomer(){
+        console.log('Save Customer');
+    }
+    handleUpdateCustomer(){
+        console.log('Update Customer');
+    }
+    handleDeleteCustomer(){
+        console.log('Delete Customer');
+    }
 }
+new CustomerController();
