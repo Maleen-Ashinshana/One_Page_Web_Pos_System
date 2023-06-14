6d4ac788699f68f5277dbfec6a36e960711a5676
Maleen Ashinshana <maleenashinshana70@gmail.com>
2023-06-14 22:19:38 +0530

    add the controller part1

---
+++ Assingmnet_07/model/PlaceOrderModel.js
@@ -0,0 +1,54 @@
+export class PlaceOrderModel{
+    constructor(ItemCode,ItemName,Price,Qty,Total) {
+        this._itemCode=ItemCode;
+        this._itemName=ItemName;
+        this._itemPrice=Price;
+        this._itemQty=Qty;
+        this._itemTotal=Total;
+        this._ItemCode = ItemCode;
+        this._ItemName = ItemName;
+        this._Price = Price;
+        this._Qty = Qty;
+        this._Total = Total;
+    }
+
+    get ItemCode() {
+        return this._ItemCode;
+    }
+
+    set ItemCode(value) {
+        this._ItemCode = value;
+    }
+
+    get ItemName() {
+        return this._ItemName;
+    }
+
+    set ItemName(value) {
+        this._ItemName = value;
+    }
+
+    get Price() {
+        return this._Price;
+    }
+
+    set Price(value) {
+        this._Price = value;
+    }
+
+    get Qty() {
+        return this._Qty;
+    }
+
+    set Qty(value) {
+        this._Qty = value;
+    }
+
+    get Total() {
+        return this._Total;
+    }
+
+    set Total(value) {
+        this._Total = value;
+    }
+}
